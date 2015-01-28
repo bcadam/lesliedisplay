95477313de732ef5731dd4e1ea7e5fdc4480574e
Adam Cragg <adam.cragg@gmail.com>
2015-01-28 17:14:24 -0500

    route

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get '*unmatched_route', :to => 'welcome#entrance'
 end
